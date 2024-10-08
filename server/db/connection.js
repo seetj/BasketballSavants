edfe228e924dd3860947b682934a76577740bdb5
seetj <jasonkseet@gmail.com>
2024-10-08 15:19:33 -0400

    functional for all players in database

--- server/db/connection.js
+++ server/db/connection.js
@@ -9,44 +9,16 @@ const client = new MongoClient(uri, {
   },
 });
 
-const databaseConnection = async () => {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (err) {
-    console.error(err);
-  }
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
+}
 
-  const playersDb = client.db("Players");
-  const boxscoresDb = client.db("BoxScores_2023");
-  return { playersDb, boxscoresDb };
-};
+const database = client.db("BoxScores_2023");
 
-export default databaseConnection;
-
-(async () => {
-  try {
-    // Call the databaseConnection function to get the databases
-    const { playersDb, boxscoresDb } = await databaseConnection();
-
-    // Test querying from the "Players" collection
-    const playersCollection = playersDb.collection("Klay Thompson");
-    const players = await playersCollection.find({}).limit(5).toArray();
-    console.log("Sample players data:", players);
-
-    // Test querying from the "BoxScores_2023" collection
-    const boxscoresCollection = boxscoresDb.collection("all_boxscores");
-    const boxscores = await boxscoresCollection.find({}).limit(5).toArray();
-    console.log("Sample boxscores data:", boxscores);
-  } catch (err) {
-    console.error("Error during testing:", err);
-  } finally {
-    // Close the MongoDB client when done
-    console.log("Connection closed.");
-  }
-})();
+export default database;
--- server/routes/stats.js
+++ server/routes/stats.js
@@ -1,126 +1,184 @@
 import express from "express";
-import databaseConnection from "../db/connection.js";
+import database from "../db/connection.js";
 
 const router = express.Router();
-
-let databases;
-
-(async () => {
-  try {
-    databases = await databaseConnection(); // Get the databases once and reuse them in routes
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  }
-})();
+const playersCollection = database.collection("players_22-23");
 
 router.get("/players/", async (req, res) => {
-  const players = await databases.playersDb.listCollections().toArray();
-  console.log(players);
-  res.send(players).status(200);
+  const names = await playersCollection
+    .find({}, { projection: { _id: 1 } })
+    .toArray();
+  res.send(names).status(200);
 });
 
-router.get("/:player", async (req, res) => {
-  let stats = await databases.playersDb.collection(`${req.params.player}`);
-  let results = await stats.find({}).toArray();
-  res.send(results).status(200);
-});
-
-router.get("/avg/", async (req, res) => {
-  const stats = databases.playersDb.collection("Klay Thompson");
-
-  const averagePoints = [
+router.get("/last10games/:player/", async (req, res) => {
+  const lastTenGames = [
     {
-      $group: {
-        _id: null,
-        averagePoints: {
-          $avg: "$PTS",
-        },
-      },
-    },
-  ];
-
-  const aggCursor = await stats.aggregate(averagePoints).toArray();
-  res.send(aggCursor).status(200);
-});
-
-router.get("/avglast10games/:player/", async (req, res) => {
-  let stats = await databases.playersDb.collection(`${req.params.player}`);
-
-  const averagePoints10 = [
-    {
-      $sort: {
-        Date: -1,
-      },
-    },
-    {
-      $limit: 10,
-    },
-    {
-      $group: {
-        _id: null,
-        avgPoints: {
-          $avg: "$PTS",
-        },
+      $match: {
+        name: `${req.params.player}`,
       },
     },
     {
       $project: {
-        _id: 0,
-        avgPoints: 1,
-      },
-    },
-  ];
-  const aggCursor = await stats.aggregate(averagePoints10).toArray();
-  res.send(aggCursor).status(200);
-});
-
-router.get("/last10games/:player/", async (req, res) => {
-  let stats = await databases.playersDb.collection(`${req.params.player}`);
-  const lastTenGames = [
-    {
-      $sort: {
-        Date: -1,
-      },
-    },
-    {
-      $limit: 10,
-    },
-  ];
-
-  const aggCursor = await stats.aggregate(lastTenGames).toArray();
-  res.send(aggCursor).status(200);
-});
-
-router.get("/boxscores/", async (req, res) => {
-  try {
-    let boxscores = await databases.boxscoresDb.collection("all_boxscoress");
-
-    // Check if the boxscores collection is available
-    if (!boxscores) {
-      return res.status(500).send("Boxscores collection not found");
-    }
-
-    console.log("Collection found");
-    const gsw = [
-      {
-        $match: {
-          $or: [
-            {
-              home: "GSW",
-            },
-            {
-              visitor: "GSW",
-            },
-          ],
+        games_played: {
+          $slice: ["$games_played", -10],
         },
       },
-    ];
-    const aggCursor = await boxscores.aggregate(gsw).toArray();
-    res.send(aggCursor).status(200);
-  } catch (err) {
-    console.error("Error fetching boxscores:", err);
-    res.status(500).send("Error fetching boxscores");
-  }
+    },
+  ];
+
+  const aggCursor = await playersCollection.aggregate(lastTenGames).toArray();
+  res.send(aggCursor).status(200);
 });
+
+router.get("/:player/averages", async (req, res) => {
+  const careerAverage = [
+    {
+      $unwind: {
+        path: "$games_played",
+      },
+    },
+    {
+      $group: {
+        _id: `${req.params.player}`,
+        avg_MP: {
+          $avg: "$games_played.stats.MP",
+        },
+        avg_FG: {
+          $avg: "$games_played.stats.FG",
+        },
+        avg_FGA: {
+          $avg: "$games_played.stats.FGA",
+        },
+        avg_3P: {
+          $avg: "$games_played.stats.'3P'",
+        },
+        avg_3PA: {
+          $avg: "$games_played.stats.'3PA'",
+        },
+        avg_PTS: {
+          $avg: "$games_played.stats.PTS",
+        },
+        avg_AST: {
+          $avg: "$games_played.stats.AST",
+        },
+        avg_REB: {
+          $avg: "$games_played.stats.REB",
+        },
+        avg_DRB: {
+          $avg: "$games_played.stats.DRB",
+        },
+        avg_ORB: {
+          $avg: "$games_played.stats.ORB",
+        },
+        avg_TOV: {
+          $avg: "$games_played.stats.TOV",
+        },
+        avg_STL: {
+          $avg: "$games_played.stats.STL",
+        },
+        avg_BLK: {
+          $avg: "$games_played.stats.BLK",
+        },
+        avg_GMSC: {
+          $avg: "$games_played.stats.GMSC",
+        },
+        avg_PluMin: {
+          $avg: "$games_played.stats.PluMin",
+        },
+      },
+    },
+  ];
+
+  const aggCursor = await playersCollection.aggregate(careerAverage).toArray();
+  console.log(aggCursor);
+  res.send(aggCursor).status(200);
+});
+// router.get("/:player", async (req, res) => {
+//   let stats = await databases.playersDb.collection(`${req.params.player}`);
+//   let results = await stats.find({}).toArray();
+//   res.send(results).status(200);
+// });
+
+// router.get("/avg/", async (req, res) => {
+//   const stats = databases.playersDb.collection("Klay Thompson");
+
+//   const averagePoints = [
+//     {
+//       $group: {
+//         _id: null,
+//         averagePoints: {
+//           $avg: "$PTS",
+//         },
+//       },
+//     },
+//   ];
+
+//   const aggCursor = await stats.aggregate(averagePoints).toArray();
+//   res.send(aggCursor).status(200);
+// });
+
+// router.get("/avglast10games/:player/", async (req, res) => {
+//   let stats = await databases.playersDb.collection(`${req.params.player}`);
+
+//   const averagePoints10 = [
+//     {
+//       $sort: {
+//         Date: -1,
+//       },
+//     },
+//     {
+//       $limit: 10,
+//     },
+//     {
+//       $group: {
+//         _id: null,
+//         avgPoints: {
+//           $avg: "$PTS",
+//         },
+//       },
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         avgPoints: 1,
+//       },
+//     },
+//   ];
+//   const aggCursor = await stats.aggregate(averagePoints10).toArray();
+//   res.send(aggCursor).status(200);
+// });
+
+// router.get("/boxscores/", async (req, res) => {
+//   try {
+//     let boxscores = await databases.boxscoresDb.collection("all_boxscoress");
+
+//     // Check if the boxscores collection is available
+//     if (!boxscores) {
+//       return res.status(500).send("Boxscores collection not found");
+//     }
+
+//     console.log("Collection found");
+//     const gsw = [
+//       {
+//         $match: {
+//           $or: [
+//             {
+//               home: "GSW",
+//             },
+//             {
+//               visitor: "GSW",
+//             },
+//           ],
+//         },
+//       },
+//     ];
+//     const aggCursor = await boxscores.aggregate(gsw).toArray();
+//     res.send(aggCursor).status(200);
+//   } catch (err) {
+//     console.error("Error fetching boxscores:", err);
+//     res.status(500).send("Error fetching boxscores");
+//   }
+// });
 
 export default router;

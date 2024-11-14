e399376dbcdf191762391f203609760d8e8555d9
seetj <jasonkseet@gmail.com>
2024-11-14 16:10:56 -0500

    editing server js file

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import stats from "./routes/stats.js";
 import boxscores from "./routes/boxscores.js";
-const PORT = process.env.PORT || 5050;
+
 const app = express();
 
 app.use(
@@ -16,7 +16,4 @@ app.use(express.json());
 app.use("/stats", stats);
 app.use("/boxscores", boxscores);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+export default app;

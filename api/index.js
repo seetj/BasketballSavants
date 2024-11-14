3c48db70fbf76b7ced34a21400852b634d6c5f5c
seetj <jasonkseet@gmail.com>
2024-11-14 16:33:31 -0500

    nore testing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import stats from "./routes/stats.js";
 import boxscores from "./routes/boxscores.js";
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
@@ -13,12 +14,17 @@ app.use(
   })
 );
 app.use(express.json());
+app.get("/test", (req, res) => {
+  res.json({ message: "Server is working" });
+});
 app.use("/stats", stats);
 app.use("/boxscores", boxscores);
 
 // start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
 
-module.exports = app;
+export default app;
